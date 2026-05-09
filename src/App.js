1982e082c2df82468dcee669c2329672e0030d7a
XDitamar <itamardanon@gmail.com>
2026-05-10 00:24:04 +0300

    Update maintenance pag

--- src/App.js
+++ src/App.js
@@ -1,15 +1,71 @@
 // src/App.js
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import Header from "./components/Header";
 
-// Only import the maintenance page
-import MaintenancePage from "./pages/MaintenancePage";
+// 🆕 (these were already in your file)
+import GoogleTranslateLoader from "./components/GoogleTranslateLoader";
+import FloatingTranslateButton from "./components/FloatingTranslateButton";
+import FloatingWhatsApp from "./components/FloatingWhatsApp";
+import AccessibilityWidget from "./components/AccessibilityWidget";
+
+// Pages
+import MaintenancePage from "./pages/MaintenancePage"; // Import the maintenance page
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import AccountPage from "./pages/AccountPage";
+import GalleryPage from "./pages/GalleryPage";
+import MePage from "./pages/MePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import AdminPage from "./pages/AdminPage";
+import ChangePasswordPage from "./pages/ChangePasswordPage";
+
+// 🆕 add these
+import ForgotPasswordPage from "./pages/ForgotPasswordPage";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 
 export default function App() {
+  const location = useLocation();
+  const [isMaintenanceMode, setIsMaintenanceMode] = useState(false);
+  
+  // Check if we're in maintenance mode (at root path with MaintenancePage)
+  useEffect(() => {
+    setIsMaintenanceMode(location.pathname === '/');
+  }, [location.pathname]);
+
   return (
-    <Routes>
-      {/* Show maintenance page at all routes */}
-      <Route path="*" element={<MaintenancePage />} />
-    </Routes>
+    <>
+      {!isMaintenanceMode && <Header />}
+      {!isMaintenanceMode && <GoogleTranslateLoader />}
+      {!isMaintenanceMode && <FloatingTranslateButton />}
+      {!isMaintenanceMode && <FloatingWhatsApp />}
+      {!isMaintenanceMode && <AccessibilityWidget />}
+
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<MaintenancePage />} /> {/* Show maintenance page at root */}
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+
+        {/* Auth routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/change-password" element={<ChangePasswordPage />} />
+
+        {/* 🆕 Reset flow */}
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+
+        {/* User / Admin */}
+        <Route path="/me" element={<MePage />} />
+        <Route path="/admin" element={<AdminPage />} />
+
+        {/* Catch-all */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
   );
 }
--- src/pages/MaintenancePage.jsx
+++ src/pages/MaintenancePage.jsx
@@ -1,29 +1,33 @@
-import React from 'react';
-import mobileImage from '../images/mob.jpeg';
+// src/pages/MaintenancePage.jsx
+import React from "react";
 
 const MaintenancePage = () => {
   return (
-    <div style={{
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      width: '100vw',
-      height: '100vh',
-      margin: 0,
-      padding: 0,
-      overflow: 'hidden',
-      backgroundColor: '#000',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <img 
-        src={mobileImage}
-        alt="האתר בתחזוקה" 
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        margin: 0,
+        padding: 0,
+        overflow: "hidden",
+        backgroundColor: "#000",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img
+        src="/mob.jpeg"
+        alt="האתר בתחזוקה"
         style={{
-          maxWidth: '100%',
-          maxHeight: '100%',
-          objectFit: 'contain'
+          width: "100%",
+          height: "100%",
+          objectFit: "contain",
+          userSelect: "none",
+          pointerEvents: "none",
         }}
       />
     </div>
