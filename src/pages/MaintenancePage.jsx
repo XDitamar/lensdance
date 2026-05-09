8cc91d7d02bd1a406f7f392a266f39d903a21a3f
XDitamar <itamardanon@gmail.com>
2026-05-10 00:33:05 +0300

    fix vercel build

--- src/pages/MaintenancePage.jsx
+++ src/pages/MaintenancePage.jsx
@@ -1,34 +1,14 @@
 // src/pages/MaintenancePage.jsx
 import React from "react";
+import "./MaintenancePage.css"; // מייבאים את קובץ העיצוב
 
 const MaintenancePage = () => {
   return (
-    <div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100vw",
-        height: "100vh",
-        margin: 0,
-        padding: 0,
-        overflow: "hidden",
-        backgroundColor: "#000",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <div className="maintenance-container">
       <img
         src="/mob.jpeg"
         alt="האתר בתחזוקה"
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "contain",
-          userSelect: "none",
-          pointerEvents: "none",
-        }}
+        className="maintenance-image"
       />
     </div>
   );
